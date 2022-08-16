20def6735df0294ecff183d282c58e1ee43dab11
Lucas Costa Pauli <lucaspauli22@gmail.com>
2022-08-16 01:03:31 -0300

    number of cards feature

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // checks if Card number can be used and reassign the cardNum variable
 function cardNumChecked() {
-    let cardNum = prompt('Com quantas cartas você quer jogar?');
-    while (((cardNum%2) !== 0) || (cardNum > 14) || (cardNum < 2 || cardNum === null)) {
-        cardNum = prompt('O número deve ser par e estar entre 2 e 14!');
+    let insertNum = prompt('Com quantas cartas você quer jogar?');
+    while (((insertNum%2) !== 0) || (insertNum > 14) || (insertNum < 2 || insertNum === null)) {
+        insertNum = prompt('O número deve ser par e estar entre 2 e 14!');
     };
-    return cardNum;
+    return insertNum;
 }
+const cardNum = cardNumChecked();
+console.log(cardNum)
 
