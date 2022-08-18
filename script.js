c109642e4a49ddd559ca25518e0177419e92d623
Lucas Costa Pauli <lucaspauli22@gmail.com>
2022-08-18 02:12:59 -0300

    version with all features working (not including bonus features)

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ function cardNumChecked() {
     };
     return insertNum;
 }
-const cardNum = cardNumChecked();
+const cardNum = Number(cardNumChecked());
 
 // shows the user the number of cards he asked for
 function showCards() {
-    for (let i=1; i<=cardNum ; i++) {
-        let card = document.querySelector(`.card${i}`);
+    for (let i=0; i<cardNum ; i++) {
+        let card = document.getElementById(i);
         card.classList.remove('hidden')
     }
 }
@@ -45,12 +45,55 @@ function sortCards() {
 };
 cardsOrder = sortCards();
 
+
 const parrots = ['<img src="./source/bobrossparrot.gif" alt="bobross parrot" />', '<img src="./source/explodyparrot.gif" alt="exploydy parrot" />', '<img src="./source/fiestaparrot.gif" alt="filesta parrot" />', '<img src="./source/metalparrot.gif" alt="metal parrot" />', '<img src="./source/revertitparrot.gif" alt="revertit parrot" />', '<img src="./source/tripletsparrot.gif" alt="triplets parrot" />', '<img src="./source/unicornparrot.gif" alt="unicorn parrot" />']
+let winningCount = 0;
+let playsCount = 0;
 
 function changeCard(element) {
-    let cardIndex = element.className
-    cardIndex = Number(cardIndex.replaceAll('card', ''));
-    let currentCard = cardsOrder[cardIndex-1];
-    element.innerHTML = parrots[currentCard-1];
-    element.classList.add('selected')
+    // allows user to click on 2 elements each time
+    const clickedCounter = document.querySelectorAll('.clicked');
+    if ((clickedCounter.length - winningCount) < 2) {
+        // add respective parrow img
+        const cardIndex = Number(element.getAttribute('id'));
+        const currentParrow = cardsOrder[cardIndex];
+        element.querySelector('.flip-card-back').innerHTML = parrots[(currentParrow-1)];
+        // add clicked to flip the card
+        element.classList.add('clicked'); 
+        // add an suport class to check the clicked ones without messing with the matched cards!
+        element.classList.add('suportClass')  
+        // add plays count
+        playsCount = playsCount + 1; 
+    }
+    checkClicked()
 }
+function checkClicked() {
+    const clickedCounter = document.querySelectorAll('.suportClass');
+    if (clickedCounter.length === (2)) {
+        //problema está no seletor, que seleciona sempre os dois ultimos da array clickedCounter, precisando selecionar os dois diferentes
+        const element1img = clickedCounter[0].querySelector('.flip-card-back img').getAttribute("src");
+        const element2img = clickedCounter[1].querySelector('.flip-card-back img').getAttribute("src");
+        clickedCounter[0].classList.remove('suportClass');
+        clickedCounter[1].classList.remove('suportClass');
+        if (element1img === element2img) {
+            winningCount = winningCount + 2;
+            clickedCounter[0].classList.add('matched');
+            clickedCounter[1].classList.add('matched');
+            setTimeout( function() {youWon()}, 600);
+        } else if (element1img !== element2img) {
+            setTimeout( function() {
+            clickedCounter[0].classList.remove('clicked');
+            clickedCounter[1].classList.remove('clicked');
+            }, 1000
+            );
+        };
+    };
+};
+
+function youWon() {
+    if (winningCount === cardNum) {
+        alert(`You won in ${playsCount} plays!`);
+    };
+};
+
+
